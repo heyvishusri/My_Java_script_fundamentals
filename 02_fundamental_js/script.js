7a2e3483e6b2c97e9f565bd74fb32eb35f5e9292
heyvishusri <vishwaskumar55347@gmail.com>
2025-01-28 23:08:41 +0530

    updated

---
+++ 02_fundamental_js/script.js
@@ -0,0 +1,50 @@
+//Activiting the strick mode:
+// ==========================
+
+
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive! : D');
+*/
+
+/*
+Flow of Execution
+The code starts with hasDriversLicense = false and passTest = true.
+The first if checks passTest (which is true), so it sets hasDriversLicense to true.
+The second if checks hasDriversLicense (now true), so it logs "I can drive! : D" to the console.
+*/
+
+
+//Functions
+// ==========================
+/*
+function logger() {
+    console.log('My name is Vishwash Kumar Sriwastava.');
+}
+
+logger(); // Call the function
+logger(); // Call the function
+logger(); // Call the function
+
+
+//Function with parameters
+// ==========================
+function fruitProcessor(apple, banana) {
+    const qnty1 = `I have ${apple} apples and ${banana} bananas.`;
+    return qnty1; // Return the string instead of assigning console.log() to qnty1
+}
+
+const appleqnty1 = fruitProcessor(5, 0);
+console.log(appleqnty1);
+
+const bananaqnty1 = fruitProcessor(0, 3);
+console.log(bananaqnty1);
+
+*/
+
+//Function Declaration vs. Expression
+// ==========================
